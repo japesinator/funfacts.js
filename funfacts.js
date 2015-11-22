1c9cf688f7fda5b603f811313b399072d6cd6817
ehennenfent <ehennenfent@imsa.edu>
2015-11-21 18:01:33 -0600

    Random Fact selection
    POC

--- funfacts.js
+++ funfacts.js
@@ -1,4 +1,6 @@
+var facts = ["Banging your head against a wall burns 150 calories an hour.", "In the UK, it is illegal to eat mince pies on Christmas Day!", "Pteronophobia is the fear of being tickled by feathers!", "When hippos are upset, their sweat turns red.", "A flock of crows is known as a murder."];
+
 window.onerror = function(message, url, lineNumber) {
-  alert("lol rekt");
+  console.log(facts[Math.floor(Math.random() * facts.length)]);
   return true;
 };
