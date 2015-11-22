15cfe30646e62677dee5e9d9dd6a985b52c76882
ehennenfent <ehennenfent@imsa.edu>
2015-11-21 18:36:56 -0600

    Pulls facts from Reddit

--- funfacts.js
+++ funfacts.js
@@ -1,6 +1,28 @@
-var facts = ["Banging your head against a wall burns 150 calories an hour.", "In the UK, it is illegal to eat mince pies on Christmas Day!", "Pteronophobia is the fear of being tickled by feathers!", "When hippos are upset, their sweat turns red.", "A flock of crows is known as a murder."];
+var facts = [
+    {data:{title:"Banging your head against a wall burns 150 calories an hour."}},
+    {data:{title:"In the UK, it is illegal to eat mince pies on Christmas Day!"}},
+    {data:{title:"Pteronophobia is the fear of being tickled by feathers!"}},
+    {data:{title:"When hippos are upset, their sweat turns red."}},
+    {data:{title:"A flock of crows is known as a murder."}},
+];
+
+var redditUrl = "https://www.reddit.com/r/funfacts/hot/.json?q=Fun%20Fact:";
+
+var xmlHttp = new XMLHttpRequest();
+xmlHttp.onreadystatechange = function() {
+    if (xmlHttp.readyState == 4 && xmlHttp.status == 200){
+        facts = JSON.parse(xmlHttp.responseText).data.children;
+    }
+}
+xmlHttp.open("GET", redditUrl, true); // true for asynchronous
+xmlHttp.send(null);
 
 window.onerror = function(message, url, lineNumber) {
-  console.log(facts[Math.floor(Math.random() * facts.length)]);
-  return true;
+    fact = facts[Math.floor(Math.random() * facts.length)].data.title;
+    try{
+        fact = fact.replace('Fun Fact','');
+    }
+    catch (err){;}
+    console.log(fact);
+    return true;
 };
